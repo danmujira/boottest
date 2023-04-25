dcc42080832113bbbb66b7802e2fc87016e1bf62
Danmuji <asemansa@naver.com>
2023-04-25 15:03:59 +0900

    rollback

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class TestController {
 	public void TestReqest(String v1) {
 		
