ca51f2122c17f0ac14cb680c307b61d711f30ba7
Danmuji <asemansa@naver.com>
2023-04-28 11:07:02 +0900

    add redis

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,13 +1,37 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.expression.Strings;
+
+import com.example.demo.service.TestService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/test")
 @Slf4j
 public class TestController {
-	public void TestReqest(String v1) {
+	private final TestService testService;
+	
+	@PostMapping("")
+	public void insertCache(@RequestBody Map<String, String> p1) {
+		log.info("req={}", p1);
+		testService.insertCache(p1.get("data"));
 		
+	}
+	
+	@GetMapping("")
+	public String getCache(@RequestParam String key) {
+		log.info("req={}", key);
+		return testService.getCache(key);
 	}
 }
